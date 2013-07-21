569ad49e9ea0df1fac3ed64f84bbe0a22b577dc2
RodolfoDella <rodolfofaquin@gmail.com>
2013-07-20 23:18:52 -0300

    Fist Commit

---
+++ sample-android/src/org/gujavasc/sample/LinearLayout.java
@@ -0,0 +1,16 @@
+package org.gujavasc.sample;
+
+import org.gujavasc.sample.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LinearLayout extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_linear_layout);
+	}
+
+}
---
+++ sample-android/src/org/gujavasc/sample/RelativeLayout.java
@@ -0,0 +1,16 @@
+package org.gujavasc.sample;
+
+import org.gujavasc.sample.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RelativeLayout extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_relative_layout);
+	}
+
+}
